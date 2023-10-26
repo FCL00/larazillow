778328d44f64b5ea52786894b5489e82c85f8a19
FCL00 <flagahit00@gmail.com>
2023-10-26 19:18:05 +0800

    initial commit

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Index</h1>
+    </div>
+    <Link href="/show">Show Page</Link>
+    <div>
+        {{ message }}
+    </div>
+   
+</template>
+
+
+<script setup>
+    import { Link } from '@inertiajs/vue3';
+   
+    defineProps({ message : String });
+</script>
+
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,9 @@
+<template>
+    <div><h1>Show</h1></div>
+    <Link href="/">Index</Link> 
+</template>
+
+<script setup>
+    import { Link } from '@inertiajs/vue3';
+</script>
+
